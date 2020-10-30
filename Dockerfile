050a7ee462c253a9a2bb6c97588fc1a984d6d017
mel-haya <yas20072003@gmail.com>
2020-10-30 01:48:32 +0100

    change mariadb to mysql

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ COPY srcs/services.sh .
 COPY srcs/localhost /etc/nginx/sites-available/
 COPY srcs/start.sql .
 COPY srcs/wordpress.sql .
-COPY srcs/latest.tar.gz .
+COPY srcs/latest.tar.gz /var/www/
 COPY srcs/mysql-apt-config_0.8.15-1_all.deb .
 
 CMD bash services.sh && tail -f /dev/null
--- srcs/services.sh
+++ srcs/services.sh
@@ -8,18 +8,31 @@ apt-get install -y php-mbstring
 
 apt-get purge -y apache2
 
+
 #mysql
-#service mysql start
-#mysql < start.sql
-#mysql wordpress -u root --password=  < wordpress.sql
+apt-get install -y gnupg
+apt-get install -y lsb-release wget
+export DEBIAN_FRONTEND=noninteractive
+echo 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7' | debconf-set-selections
+dpkg -i mysql-apt-config_0.8.15-1_all.deb
+apt-get update
+apt-get install -y mysql-server
+chown -R mysql: /var/lib/mysql
+service mysql start
+
+#init database
+
+mysql -u root -e "CREATE DATABASE wordpress;";
+mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';"
+mysql -u root -e "FLUSH PRIVILEGES;";
+mysql -u root -e "UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE user='root';";
+mysql wordpress -u root --password=  < wordpress.sql
 
 #ssl
 apt-get install -y openssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj '/C=MA/ST=rhamna/L=Bengruir/O=1337/CN=localhost' -keyout ./root/localhost.key -out ./root/localhost.crt
 
 #wordpress
-cp latest.tar.gz /var/www
-rm -f latest.tar.gz
 cd /var/www
 tar -xf latest.tar.gz
 rm -f latest.tar.gz
